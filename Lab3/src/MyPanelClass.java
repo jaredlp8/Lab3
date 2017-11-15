0a50a62cdf25ba096aa41233ebc5208bc1145696
4015e11 <4015e11@granada.ece.uprm.edu>
2017-11-15 11:38:06 -0400

    Created flag of Puerto Rico

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -56,19 +56,57 @@ public class MyPanelClass extends JPanel {
 //                        g.setColor(Color.YELLOW);
 //                        g.fillPolygon(p);
                         
-                        Polygon p2 = new Polygon();
-                        p2.addPoint(x1 + 25, y1 + 73);
-                        p2.addPoint(x1 + 41, y1 + 73);
-                        p2.addPoint(x1 + 47, y1 + 58);
-                        p2.addPoint(x1 + 53, y1 + 73);
-                        p2.addPoint(x1 + 69, y1 + 73);
-                        p2.addPoint(x1 + 56, y1 + 83);
-                        p2.addPoint(x1 + 61, y1 + 98);
-                        p2.addPoint(x1 + 47, y1 + 88);
-                        p2.addPoint(x1 + 34, y1 + 98);
-                        p2.addPoint(x1 + 38, y1 + 83);
+                     // Stripes
+
+                        g.setColor(Color.RED);
+                        g.fillRect(x1, y1, width +1 , height/5);
                         g.setColor(Color.WHITE);
-                        g.drawPolygon(p2);
+                        g.fillRect(x1, y1 + 33, width +1 , height/5);
+                        g.setColor(Color.RED); 
+                        g.fillRect(x1, y1 + 66, width + 1, height/5);
+                        g.setColor(Color.WHITE);
+                        g.fillRect(x1, y1 + 99, width + 1, height/5);
+                        g.setColor(Color.RED);
+                        g.fillRect(x1, y1 + 132, width + 1, height/5);
+
+
+                         // Triangle
+
+                      Polygon p3 = new Polygon();
+                      p3.addPoint(x1, y1);
+                      p3.addPoint(x2/2, y2/2);
+                      p3.addPoint(x1, y2);
+                      g.setColor(Color.BLUE);
+                      g.fillPolygon(p3);
+
+                      
+                      Polygon p5 = new Polygon();
+                      p5.addPoint(x1 + 25, y1 + 73);
+                      p5.addPoint(x1 + 41, y1 + 73);
+                      p5.addPoint(x1 + 34, y1 + 98);
+                      p5.addPoint(x1 + 38, y1 + 83);
+                      g.setColor(Color.WHITE); 
+                      g.drawPolygon(p5);
+                      g.fillPolygon(p5);
+
+                    //Star
+                      Polygon p2 = new Polygon();
+                      p2.addPoint(x1 + 25, y1 + 73);
+                      p2.addPoint(x1 + 41, y1 + 73);
+                      p2.addPoint(x1 + 47, y1 + 58);
+                      p2.addPoint(x1 + 53, y1 + 73);
+                      p2.addPoint(x1 + 69, y1 + 73);
+                      p2.addPoint(x1 + 56, y1 + 83);
+                      p2.addPoint(x1 + 61, y1 + 98);
+                      p2.addPoint(x1 + 47, y1 + 88);
+                      p2.addPoint(x1 + 34, y1 + 98);
+                      p2.addPoint(x1 + 38, y1 + 83);
+                      g.setColor(Color.WHITE);
+                      g.fillPolygon(p2);                        
             }
 }
 
+       
+
+                               
+                      
